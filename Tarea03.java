8f5744a092804d5729e24a9c6f7b14c3334ea5e3
sebascrd13 <106779944+sebascrd13@users.noreply.github.com>
2022-09-28 00:12:29 -0600

    Update Tarea03.java

--- Tarea03.java
+++ Tarea03.java
@@ -1,6 +1,41 @@
-public class Tarea03 {
+import javax.swing.JOptionPane;
 
-    public static void name(String[] args) {
-        
+public class Tarea03 {
+    public static void main(String[] args) {
+//===========================WHILE================================== 
+        boolean flag = true;
+        byte num1;
+        byte num2;
+
+        while (flag) {
+            num1 = Byte.parseByte(JOptionPane.showInputDialog(
+                "Digite un número: "));
+            num2 = Byte.parseByte(JOptionPane.showInputDialog(
+                "Digite otro número: "));
+            if (num1 == num2){
+                flag=false;
+                JOptionPane.showMessageDialog(null,
+                "Los números son iguales!!");
+            }else{
+                JOptionPane.showMessageDialog(null,
+                "Los números son diferentes");
+            }
+        }
+
+//==========================DO WHILE===============================
+
+
+
+
+
+
+
+
+
+
+
+
+//===============================FOR=================================
+
     }
 }
